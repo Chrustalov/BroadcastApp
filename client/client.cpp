f2105f8b99b5fee1ff2614ab7649e7c75de7ed65
Uppercode <oleksiwe22@gmail.com>
2023-12-06 23:09:37 +0200

    setup project

---
+++ client/client.cpp
@@ -0,0 +1,15 @@
+#include "client.h"
+#include "./ui_client.h"
+
+client::client(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::client)
+{
+    ui->setupUi(this);
+}
+
+client::~client()
+{
+    delete ui;
+}
+
